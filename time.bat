aa8e3e7a04bb0f71290304594e568af3aadcbbcd
Zerstorer23 <bismark439@gmail.com>
2022-05-26 21:17:00 -0500

    2022-05-26 _오후 0916

--- time.bat
+++ time.bat
@@ -1,7 +1,7 @@
 For /f "tokens=1-3 delims=/-" %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 SET timestr=%mydate%_%mytime%
-git pull
-git add .
-git commit -m "%timestr%"
-git push
+%mydate%
+%mytime%
+%timestr%
+timeout -1
