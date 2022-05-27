4ba18a92362c19c3331ea49533e5e497a8149c86
Zerstorer23 <bismark439@gmail.com>
2022-05-26 21:17:36 -0500

    2022-05-26 _오후 09:17

--- gitpush.bat
+++ gitpush.bat
@@ -1,5 +1,5 @@
 For /f "tokens=1-3 delims=/-" %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
 SET timestr=%mydate%_%mytime%
 git pull
 git add .
