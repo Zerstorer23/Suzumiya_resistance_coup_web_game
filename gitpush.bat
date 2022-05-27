a8e93cd48c96d44666cd4a9c54ae716b238b24fe
Zerstorer23 <bismark439@gmail.com>
2022-05-26 21:05:27 -0500

    today

--- gitpush.bat
+++ gitpush.bat
@@ -1,4 +1,6 @@
-
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+echo %mydate%_%mytime%
 git pull
 git add .
 git commit -m "today"
