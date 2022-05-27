a8aef702b173dfa89bb30570268bba0fb0b9f835
Zerstorer23 <bismark439@gmail.com>
2022-05-26 21:17:59 -0500

    2022-05-26 _오후 09:17

--- time.bat
+++
@@ -1,7 +0,0 @@
-For /f "tokens=1-3 delims=/-" %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-SET timestr=%mydate%_%mytime%
-%mydate%
-%mytime%
-%timestr%
-timeout -1
