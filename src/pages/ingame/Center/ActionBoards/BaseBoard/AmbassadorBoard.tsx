44e8ed188be24827ab5281173d079ab2f8d7084b
jzhang2254 <jzhang2254@wisc.edu>
2022-05-29 17:11:57 -0500

    Sun 05-29-2022 _05:11 PM

--- src/pages/ingame/Center/ActionBoards/BaseBoard/AmbassadorBoard.tsx
+++ src/pages/ingame/Center/ActionBoards/BaseBoard/AmbassadorBoard.tsx
@@ -2,7 +2,44 @@ import BaseActionButton from "pages/ingame/Center/ActionBoards/BaseBoard/BaseAct
 import classes from "pages/ingame/Center/ActionBoards/BaseBoard/BaseBoard.module.css";
 import { ActionInfo } from "system/GameStates/ActionInfo";
 import { ActionType } from "system/GameStates/States";
+import RoomContext from "system/context/room-context";
+import { useContext } from "react";
+import LocalContext, {
+  LocalField,
+} from "system/context/localInfo/local-context";
+import { getSortedListFromMap } from "system/GameStates/RoomGenerator";
+import { DeckManager } from "system/cards/DeckManager";
+import { Card } from "system/cards/Card";
+
 export default function AmbassadorBoard(): JSX.Element {
+  const ctx = useContext(RoomContext);
+  const playerMap = ctx.room.playerMap;
+  const deck = ctx.room.game.deck;
+  const localCtx = useContext(LocalContext);
+  const myId: string = localCtx.getVal(LocalField.Id);
+  let arr = deck.split(",");
+  const myPlayer = playerMap.get(myId);
+  const sortedList: string[] = getSortedListFromMap(playerMap);
+  //get 2 cards from top of the deck
+  let max = 0;
+  for (let id in sortedList) {
+    if (playerMap.get(id)!.icard >= max) {
+      max = playerMap.get(id)!.icard;
+    }
+  }
+  max += 2;
+
+  let charArr = [
+    arr[myPlayer!.icard],
+    arr[myPlayer!.icard + 1],
+    arr[max],
+    arr[max + 1],
+  ];
+
+  const cardArr: Card[] = charArr.map((val) => {
+    return DeckManager.getCardFromChar(val);
+  });
+
   //TODO Show cards and accept two
   const actions = [
     ActionType.Accept,
