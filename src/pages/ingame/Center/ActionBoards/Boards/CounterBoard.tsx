83b85ad35490d8a7e6219694dd40ca2e6f1a574a
Zerstorer23 <bismark439@gmail.com>
2022-05-31 15:27:33 -0500

    intellij support

--- src/pages/ingame/Center/ActionBoards/Boards/CounterBoard.tsx
+++ src/pages/ingame/Center/ActionBoards/Boards/CounterBoard.tsx
@@ -1,65 +1,63 @@
 import BaseActionButton from "pages/ingame/Center/ActionBoards/Boards/BaseActionButton";
 import classes from "pages/ingame/Center/ActionBoards/Boards/BaseBoard.module.css";
-import { useContext } from "react";
+import {useContext} from "react";
 import LocalContext, {
-  LocalField,
+    LocalField,
 } from "system/context/localInfo/local-context";
 import RoomContext from "system/context/room-context";
-import { ActionInfo } from "system/GameStates/ActionInfo";
-import { GameManager } from "system/GameStates/GameManager";
-import { ActionType, StateManager } from "system/GameStates/States";
-import { TurnManager } from "system/GameStates/TurnManager";
+import {ActionInfo} from "system/GameStates/ActionInfo";
+import {ActionType, StateManager} from "system/GameStates/States";
+import {TurnManager} from "system/GameStates/TurnManager";
+import {pushIsALieState} from "pages/ingame/Center/ActionBoards/Boards/Solver/IsALieSolver";
+
 export default function CounterBoard(): JSX.Element {
-  //TODO change by board state
-  const actions = [ActionType.Accept, ActionType.IsALie];
-  /*
-      case BoardState.CalledGetThree:
-      case BoardState.CalledChangeCards:
-      case BoardState.CalledSteal:
-      case BoardState.CalledAssassinate:
-      case BoardState.AidBlocked:
-      case BoardState.StealBlocked:
-      case BoardState.AssassinBlocked:
-*/
-  const ctx = useContext(RoomContext);
-  const localCtx = useContext(LocalContext);
-  const myId = localCtx.getVal(LocalField.Id);
+    //TODO change by board state
+    const actions = [ActionType.Accept, ActionType.IsALie];
+    /*
+        case BoardState.CalledGetThree:
+        case BoardState.CalledChangeCards:
+        case BoardState.CalledSteal:
+        case BoardState.CalledAssassinate:
+        case BoardState.AidBlocked:
+        case BoardState.StealBlocked:
+        case BoardState.AssassinBlocked:
+  */
+    const ctx = useContext(RoomContext);
+    const localCtx = useContext(LocalContext);
+    const myId = localCtx.getVal(LocalField.Id);
 
-  function onMakeAction(action: ActionType) {
-    const board = ctx.room.game.state.board;
-    const gameAction = GameManager.copyGameAction(ctx.room.game.action);
-    let newBoard = null;
-    if (action === ActionType.IsALie) {
-      newBoard = StateManager.getChallengedState(board);
-      gameAction.challengerId = myId;
-    } else {
-      //only a few case actually triggers accepted state
-      //when is my turn && state is block type
-      const isMyTurn = TurnManager.isMyTurn(ctx, localCtx);
-      if (!isMyTurn) return;
-      if (!StateManager.isBlockedState(board)) return;
-      newBoard = StateManager.getAcceptedState(board);
+    function onMakeAction(action: ActionType) {
+        const board = ctx.room.game.state.board;
+        let newBoard = null;
+        if (action === ActionType.IsALie) {
+            pushIsALieState(ctx, myId);
+            return;
+        }
+        const isMyTurn = TurnManager.isMyTurn(ctx, localCtx);
+        if (!isMyTurn) return;
+        if (!StateManager.isBlockedState(board)) return;
+        newBoard = StateManager.getAcceptedState(board);
+
+        if (newBoard === null) return;
+        StateManager.createState(ctx.room.game.state, newBoard);
     }
-    if (newBoard === null) return;
-    return StateManager.createState(ctx.room.game.state, newBoard);
-  }
 
-  return (
-    <div className={classes.container}>
-      {actions.map((action: ActionType, index: number) => {
-        const baseIndex = index + 1;
-        const cssName = classes[`cell${baseIndex}`];
-        return (
-          <BaseActionButton
-            key={index}
-            className={`${cssName}`}
-            param={new ActionInfo(action)}
-            onClickButton={() => {
-              onMakeAction(action);
-            }}
-          />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className={classes.container}>
+            {actions.map((action: ActionType, index: number) => {
+                const baseIndex = index + 1;
+                const cssName = classes[`cell${baseIndex}`];
+                return (
+                    <BaseActionButton
+                        key={index}
+                        className={`${cssName}`}
+                        param={new ActionInfo(action)}
+                        onClickButton={() => {
+                            onMakeAction(action);
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
 }
--- src/pages/ingame/Center/ActionBoards/Boards/ReactAssassinBoard.tsx
+++ src/pages/ingame/Center/ActionBoards/Boards/ReactAssassinBoard.tsx
@@ -1,56 +1,62 @@
 import BaseActionButton from "pages/ingame/Center/ActionBoards/Boards/BaseActionButton";
 import classes from "pages/ingame/Center/ActionBoards/Boards/BaseBoard.module.css";
-import { useContext } from "react";
+import {useContext} from "react";
 import LocalContext, {
-  LocalField,
+    LocalField,
 } from "system/context/localInfo/local-context";
 import RoomContext from "system/context/room-context";
-import { ActionInfo } from "system/GameStates/ActionInfo";
-import { GameManager } from "system/GameStates/GameManager";
-import { ActionType, BoardState, StateManager } from "system/GameStates/States";
+import {ActionInfo} from "system/GameStates/ActionInfo";
+import {GameManager} from "system/GameStates/GameManager";
+import {ActionType, BoardState, StateManager} from "system/GameStates/States";
+import {pushIsALieState} from "pages/ingame/Center/ActionBoards/Boards/Solver/IsALieSolver";
+
 export default function ReactAssassinBoard(): JSX.Element {
-  const ctx = useContext(RoomContext);
-  const localCtx = useContext(LocalContext);
-  const myId = localCtx.getVal(LocalField.Id);
+    const ctx = useContext(RoomContext);
+    const localCtx = useContext(LocalContext);
+    const myId = localCtx.getVal(LocalField.Id);
 
-  const actions = [
-    ActionType.Accept,
-    ActionType.IsALie,
-    ActionType.ContessaBlocksAssassination,
-  ];
+    const actions = [
+        ActionType.Accept,
+        ActionType.IsALie,
+        ActionType.ContessaBlocksAssassination,
+    ];
 
-  function onMakeAction(action: ActionType) {
-    const board = ctx.room.game.state.board;
-    const gameAction = GameManager.copyGameAction(ctx.room.game.action);
-    let newBoard = null;
-    if (action === ActionType.IsALie) {
-      newBoard = StateManager.getChallengedState(board);
-      gameAction.challengerId = myId;
-    } else if (action === ActionType.Accept) {
-      newBoard = StateManager.getAcceptedState(board);
-    } else if (action === ActionType.ContessaBlocksAssassination) {
-      newBoard = BoardState.AssassinBlocked;
+    function onMakeAction(action: ActionType) {
+        const board = ctx.room.game.state.board;
+        let newBoard = null;
+        switch (action) {
+            case ActionType.IsALie:
+                pushIsALieState(ctx, myId);
+                return;
+            case ActionType.Accept:
+                newBoard = StateManager.getAcceptedState(board);
+                break;
+            case ActionType.ContessaBlocksAssassination:
+                newBoard = BoardState.AssassinBlocked;
+                break;
+            default:
+                return;
+        }
+        if (newBoard === null) return;
+        StateManager.createState(ctx.room.game.state, newBoard);
     }
-    if (newBoard === null) return;
-    StateManager.createState(ctx.room.game.state, newBoard);
-  }
 
-  return (
-    <div className={classes.container}>
-      {actions.map((action: ActionType, index: number) => {
-        const baseIndex = index + 1;
-        const cssName = classes[`cell${baseIndex}`];
-        return (
-          <BaseActionButton
-            key={index}
-            className={`${cssName}`}
-            param={new ActionInfo(action)}
-            onClickButton={() => {
-              onMakeAction(action);
-            }}
-          />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className={classes.container}>
+            {actions.map((action: ActionType, index: number) => {
+                const baseIndex = index + 1;
+                const cssName = classes[`cell${baseIndex}`];
+                return (
+                    <BaseActionButton
+                        key={index}
+                        className={`${cssName}`}
+                        param={new ActionInfo(action)}
+                        onClickButton={() => {
+                            onMakeAction(action);
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
 }
