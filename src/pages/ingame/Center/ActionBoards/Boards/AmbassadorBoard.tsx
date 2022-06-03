03db50dd2143e50e4fb3208c6ebf8babcdd23675
jzhang2254 <jzhang2254@wisc.edu>
2022-06-03 18:49:32 -0500

    Fri 06-03-2022 _06:49 PM

--- src/pages/ingame/Center/ActionBoards/Boards/AmbassadorBoard.tsx
+++ src/pages/ingame/Center/ActionBoards/Boards/AmbassadorBoard.tsx
@@ -1,130 +1,143 @@
 import classes from "pages/ingame/Center/ActionBoards/Boards/BaseBoard.module.css";
 import RoomContext from "system/context/roomInfo/room-context";
-import {Fragment, useContext, useState} from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import LocalContext from "system/context/localInfo/local-context";
-import {DeckManager} from "system/cards/DeckManager";
-import {Card, CardRole} from "system/cards/Card";
+import { DeckManager } from "system/cards/DeckManager";
+import { Card, CardRole } from "system/cards/Card";
 import BaseActionButton from "pages/ingame/Center/ActionBoards/Boards/ActionButtons/BaseActionButton";
-import {TurnManager} from "system/GameStates/TurnManager";
+import { TurnManager } from "system/GameStates/TurnManager";
 import * as ActionManager from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
-import {TransitionAction} from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
+import { TransitionAction } from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
 
 export default function AmbassadorBoard(): JSX.Element {
-    const ctx = useContext(RoomContext);
-    const playerMap = ctx.room.playerMap;
-    const deck: CardRole[] = ctx.room.game.deck;
-    const localCtx = useContext(LocalContext);
-    const [myId, myPlayer] = TurnManager.getMyInfo(ctx, localCtx);
-    //get 2 cards from top of the deck
-    const topIndex = DeckManager.peekTopIndex(ctx);
+  const ctx = useContext(RoomContext);
+  const playerMap = ctx.room.playerMap;
+  const deck: CardRole[] = ctx.room.game.deck;
 
-    /**************************************************************
-     * Handle the case where deck only has 0 ~ 2 cards available  *
-     **************************************************************/
-    let charArr = [
-        ...DeckManager.peekCards(deck, myPlayer.icard, 2),
-        ...DeckManager.peekCards(deck, topIndex, 2),
-    ];
+  const localCtx = useContext(LocalContext);
+  const [myId, myPlayer] = TurnManager.getMyInfo(ctx, localCtx);
+  //get 2 cards from top of the deck
+  const topIndex = DeckManager.peekTopIndex(ctx);
 
-    const cardArr: Card[] = charArr.map((val) => {
-        return DeckManager.getCardFromChar(val);
-    });
+  /**************************************************************
+   * Handle the case where deck only has 0 ~ 2 cards available  *
+   **************************************************************/
+  let charArr = [
+    ...DeckManager.peekCards(deck, myPlayer.icard, 2),
+    ...DeckManager.peekCards(deck, topIndex, 2),
+  ];
 
-    const [firstCardPicked, setFirstCardPicked] = useState<boolean>(false);
+  const cardArr: Card[] = charArr.map((val) => {
+    return DeckManager.getCardFromChar(val);
+  });
 
-    //TODO: Player with 1 card can only coose 1 card
-    /***
-     * How the swap works
-     * prompt max 4 choices
-     * user can pick max 2
-     * First click
-     *      -Put the selected card in icard 0
-     *          -Original icard goes to selected.
-     *       Second Click
-     *         -    Same thing
-     *        END STATE. END TURN.
-     *        (push the deck)
-     * @param action
-     */
-    function onMakeAction(action: Card) {
-        if (!firstCardPicked) {
-            setFirstCardPicked(true);
-            switch (action.cardRole) {
-                case deck[myPlayer!.icard + 1]:
-                    DeckManager.swap(myPlayer!.icard + 1, myPlayer!.icard, deck);
-                    break;
-                case deck[topIndex]:
-                    DeckManager.swap(topIndex, myPlayer!.icard, deck);
-                    break;
-                case deck[topIndex + 1]:
-                    DeckManager.swap(topIndex + 1, myPlayer!.icard, deck);
-                    break;
-            }
-            DeckManager.pushDeck(ctx, deck);
-            console.log(DeckManager.playerCardNum(deck, myPlayer.icard));
-            if (DeckManager.playerCardNum(deck, myPlayer.icard) === 1) {
-                ActionManager.prepareAndPushState(ctx, (newAction, newState) => {
-                    return TransitionAction.EndTurn;
-                });
-            }
-        } else if (firstCardPicked) {
-            setFirstCardPicked(false);
-            switch (action.cardRole) {
-                case deck[topIndex]:
-                    DeckManager.swap(topIndex, myPlayer!.icard + 1, deck);
-                    break;
-                case deck[topIndex + 1]:
-                    DeckManager.swap(topIndex + 1, myPlayer!.icard + 1, deck);
-                    break;
-            }
-            DeckManager.pushDeck(ctx, deck);
-            ActionManager.prepareAndPushState(ctx, (newAction, newState) => {
-                return TransitionAction.EndTurn;
-            });
-        }
+  const [firstCardPicked, setFirstCardPicked] = useState<number>(-1);
+
+  useEffect(() => {
+    if (DeckManager.playerCardNum(deck, myPlayer.icard) === 1) {
+      //swap
+      if (firstCardPicked === 1) {
+        DeckManager.swap(myPlayer!.icard + 1, myPlayer!.icard, deck);
+      } else if (firstCardPicked === 2) {
+        DeckManager.swap(topIndex, myPlayer!.icard, deck);
+      } else if (firstCardPicked === 3) {
+        DeckManager.swap(topIndex + 1, myPlayer!.icard, deck);
+      }
+      ActionManager.prepareAndPushState(ctx, (newAction, newState) => {
+        DeckManager.pushDeck(ctx, deck);
+        return TransitionAction.EndTurn;
+      });
     }
+  }, [firstCardPicked]);
 
-    //ADD SELCETED CSS STYLE
-    return (
-        <Fragment>
-            <div className={classes.header}>
-                Choose 2 cards that you want to keep...
-            </div>
-            <p>mine</p>
-            <div className={classes.quarterContainer}>
-                {cardArr.map((action: Card, index: number) => {
-                    const baseIndex = index + 1;
-                    const cssName = classes[`cell${baseIndex}`];
-                    return (
-                        <BaseActionButton
-                            key={index}
-                            className={`${cssName}`}
-                            param={action}
-                            onClickButton={() => {
-                                onMakeAction(action);
-                            }}
-                        />
-                    );
-                })}
-            </div>
-            <p>deck</p>
+  function onMakeAction(index: number) {
+    if (firstCardPicked === -1) {
+      setFirstCardPicked(index);
+    } else if (firstCardPicked !== -1) {
+      if (firstCardPicked === 1) {
+        DeckManager.swap(myPlayer!.icard + 1, myPlayer!.icard, deck);
+      } else if (firstCardPicked === 2) {
+        DeckManager.swap(topIndex, myPlayer!.icard, deck);
+        if (index === 0) {
+          DeckManager.swap(topIndex, myPlayer!.icard + 1, deck);
+        }
+      } else if (firstCardPicked === 3) {
+        DeckManager.swap(topIndex + 1, myPlayer!.icard, deck);
+        if (index === 0) {
+          DeckManager.swap(topIndex + 1, myPlayer!.icard + 1, deck);
+        }
+      }
 
-            <div className={classes.quarterContainer}>
-                {cardArr.map((action: Card, index: number) => {
-                    const baseIndex = index + 1;
-                    const cssName = classes[`cell${baseIndex}`];
-                    return (
-                        <BaseActionButton
-                            key={index}
-                            className={`${cssName}`}
-                            param={action}
-                            onClickButton={() => {
-                                onMakeAction(action);
-                            }}
-                        />
-                    );
-                })}
-            </div>
-        </Fragment>
-    );
+      switch (index) {
+        case 2:
+          DeckManager.swap(topIndex, myPlayer!.icard + 1, deck);
+          break;
+        case 3:
+          DeckManager.swap(topIndex + 1, myPlayer!.icard + 1, deck);
+          break;
+      }
+      DeckManager.pushDeck(ctx, deck);
+      ActionManager.prepareAndPushState(ctx, (newAction, newState) => {
+        return TransitionAction.EndTurn;
+      });
+    }
+  }
+
+  //ADD SELCETED CSS STYLE
+  return (
+    <Fragment>
+      <div className={classes.header}>
+        Choose 2 cards that you want to keep...
+      </div>
+      <p>mine</p>
+      <div className={classes.quarterContainer}>
+        {cardArr.map((action: Card, index: number) => {
+          console.log(index);
+          if (firstCardPicked === index) {
+            return <Fragment />;
+          }
+          if (index > 1) {
+            return <Fragment key={index} />;
+          }
+
+          const baseIndex = index + 1;
+          const cssName = classes[`cell${baseIndex}`];
+          return (
+            <BaseActionButton
+              key={index}
+              className={`${cssName}`}
+              param={action}
+              onClickButton={() => {
+                onMakeAction(index);
+              }}
+            />
+          );
+        })}
+      </div>
+      <p>deck</p>
+
+      <div className={classes.quarterContainer}>
+        {cardArr.map((action: Card, index: number) => {
+          if (firstCardPicked === index) {
+            return <Fragment />;
+          }
+          if (index < 2) {
+            return <Fragment key={index} />;
+          }
+          const baseIndex = index - 1;
+          const cssName = classes[`cell${baseIndex}`];
+          return (
+            <BaseActionButton
+              key={index}
+              className={`${cssName}`}
+              param={action}
+              onClickButton={() => {
+                onMakeAction(index);
+              }}
+            />
+          );
+        })}
+      </div>
+    </Fragment>
+  );
 }
--- src/pages/ingame/Left/MyBoard/MyBoard.tsx
+++ src/pages/ingame/Left/MyBoard/MyBoard.tsx
@@ -2,55 +2,58 @@ import classes from "./MyBoard.module.css";
 import gc from "global.module.css";
 import VerticalLayout from "pages/components/ui/VerticalLayout";
 import MyCardComponent from "pages/ingame/Left/MyBoard/MyCardComponent/MyCardComponent";
-import {Card} from "system/cards/Card";
+import { Card } from "system/cards/Card";
 import CoinDisplayComponent from "pages/ingame/Left/MyBoard/CoinDisplayComponent/CoinDisplayComponent";
-import {Fragment, useContext} from "react";
-import LocalContext, {LocalField,} from "system/context/localInfo/local-context";
-import {Player} from "system/GameStates/GameTypes";
+import { Fragment, useContext } from "react";
+import LocalContext, {
+  LocalField,
+} from "system/context/localInfo/local-context";
+import { Player } from "system/GameStates/GameTypes";
 import RoomContext from "system/context/roomInfo/room-context";
-import {DeckManager} from "system/cards/DeckManager";
-import {CursorState} from "system/context/localInfo/LocalContextProvider";
+import { DeckManager } from "system/cards/DeckManager";
+import { CursorState } from "system/context/localInfo/LocalContextProvider";
 
 export default function MyBoard(): JSX.Element {
-    //useContext Room context
-    //localContext
-    //get myId from local
-    //get myPlyaer from room
-    //put data below
+  //useContext Room context
+  //localContext
+  //get myId from local
+  //get myPlyaer from room
+  //put data below
 
-    const ctx = useContext(RoomContext);
-    const localCtx = useContext(LocalContext);
-    const myId: string = localCtx.getVal(LocalField.Id);
-    const myPlayer: Player = ctx.room.playerMap.get(myId)!;
-    const cardIndex = myPlayer.icard;
-    const myCoin = myPlayer.coins;
-    const deck = ctx.room.game.deck;
-    let firstChar = deck[cardIndex];
-    let secondChar = deck[cardIndex + 1];
-    let charArr = [firstChar, secondChar];
+  const ctx = useContext(RoomContext);
+  const localCtx = useContext(LocalContext);
+  const myId: string = localCtx.getVal(LocalField.Id);
+  const myPlayer: Player = ctx.room.playerMap.get(myId)!;
+  const cardIndex = myPlayer.icard;
+  const myCoin = myPlayer.coins;
+  const deck = ctx.room.game.deck;
+  let firstChar = deck[cardIndex];
+  let secondChar = deck[cardIndex + 1];
+  let charArr = [firstChar, secondChar];
 
-    const cardArr: Card[] = charArr.map((val) => {
-        return DeckManager.getCardFromChar(val);
-    });
-    const showCards =
-        localCtx.getVal(LocalField.TutorialSelector) === CursorState.Idle;
-    return (
-        <div className={`${gc.round_border} ${classes.container}`}>
-            <VerticalLayout>
-                <div className={classes.infoContainer}>
-                    {showCards && (
-                        <Fragment>
-                            <p>My Cards</p>
-                            <MyCardComponent card={cardArr[0]}/>
-                            <MyCardComponent card={cardArr[1]}/>
-                        </Fragment>
-                    )}
-                    {!showCards && (
-                        <div className={classes.tutorialContainer}>Tutorial board</div>
-                    )}
-                </div>
-                <CoinDisplayComponent coins={myCoin}/>
-            </VerticalLayout>
+  const cardArr: Card[] = charArr.map((val) => {
+    return DeckManager.getCardFromChar(val);
+  });
+
+  const showCards =
+    localCtx.getVal(LocalField.TutorialSelector) === CursorState.Idle;
+  return (
+    <div className={`${gc.round_border} ${classes.container}`}>
+      <VerticalLayout>
+        <div className={classes.infoContainer}>
+          {showCards && (
+            <Fragment>
+              <p>My Cards</p>
+              <MyCardComponent card={cardArr[0]} />
+              <MyCardComponent card={cardArr[1]} />
+            </Fragment>
+          )}
+          {!showCards && (
+            <div className={classes.tutorialContainer}>Tutorial board</div>
+          )}
         </div>
-    );
+        <CoinDisplayComponent coins={myCoin} />
+      </VerticalLayout>
+    </div>
+  );
 }
