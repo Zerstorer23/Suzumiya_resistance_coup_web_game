b46867ef7c973bf75a494d7ca870394a770e50b5
jzhang2254 <jzhang2254@wisc.edu>
2022-06-03 20:32:24 -0500

    Fri 06-03-2022 _08:32 PM

--- src/system/cards/DeckManager.tsx
+++ src/system/cards/DeckManager.tsx
@@ -1,147 +1,150 @@
-import {CardDeck, CardRole} from "system/cards/Card";
-import {shuffleArray} from "system/GameConstants";
-import {Player} from "system/GameStates/GameTypes";
-import {DbReferences, ReferenceManager} from "system/Database/RoomDatabase";
-import {CardPool} from "system/cards/CardPool";
-import {RoomContextType} from "system/context/roomInfo/RoomContextProvider";
+import { CardDeck, CardRole } from "system/cards/Card";
+import { shuffleArray } from "system/GameConstants";
+import { Player } from "system/GameStates/GameTypes";
+import { DbReferences, ReferenceManager } from "system/Database/RoomDatabase";
+import { CardPool } from "system/cards/CardPool";
+import { RoomContextType } from "system/context/roomInfo/RoomContextProvider";
 
 /*
 Manager file that helps decoding deck string into cards
 */
 export const DeckManager = {
-    isDead(role: CardRole): boolean {
-        switch (role) {
-            case CardRole.DEAD_Duke:
-            case CardRole.DEAD_Captain:
-            case CardRole.DEAD_Assassin:
-            case CardRole.DEAD_Contessa:
-            case CardRole.DEAD_Ambassador:
-            case CardRole.None:
-            case undefined:
-                return true;
-            default:
-                return false;
-        }
-    },
-    playerIsDead(deck: CardDeck, player: Player): boolean {
-        const cards = this.peekCards(deck, player.icard, 2);
-        return this.isDead(cards[0]) && this.isDead(cards[1]);
+  isDead(role: CardRole): boolean {
+    switch (role) {
+      case CardRole.DEAD_Duke:
+      case CardRole.DEAD_Captain:
+      case CardRole.DEAD_Assassin:
+      case CardRole.DEAD_Contessa:
+      case CardRole.DEAD_Ambassador:
+      case CardRole.None:
+      case undefined:
+        return true;
+      default:
+        return false;
+    }
+  },
+  playerIsDead(deck: CardDeck, player: Player): boolean {
+    const cards = this.peekCards(deck, player.icard, 2);
+    return this.isDead(cards[0]) && this.isDead(cards[1]);
+  },
+  playerHasCard(deck: CardDeck, card: CardRole, player: Player): boolean {
+    const cards = this.peekCards(deck, player.icard, 2);
+    return cards[0] === card || cards[1] === card;
+  },
+  getRoleFromChar(val: string): CardRole {
+    switch (val) {
+      case CardRole.None:
+      case CardRole.Duke:
+      case CardRole.Captain:
+      case CardRole.Assassin:
+      case CardRole.Ambassador:
+      case CardRole.Contessa:
+      case CardRole.DEAD_Captain:
+      case CardRole.DEAD_Duke:
+      case CardRole.DEAD_Assassin:
+      case CardRole.DEAD_Ambassador:
+      case CardRole.DEAD_Contessa:
+        return val as CardRole;
+      default:
+        return CardRole.None;
+    }
+  },
+  killCardAt(deck: CardRole[], index: number) {
+    const role = deck[index];
+    switch (role) {
+      case CardRole.Duke:
+        deck[index] = CardRole.DEAD_Duke;
+        break;
+      case CardRole.Captain:
+        deck[index] = CardRole.DEAD_Captain;
+        break;
+      case CardRole.Assassin:
+        deck[index] = CardRole.DEAD_Assassin;
+        break;
+      case CardRole.Ambassador:
+        deck[index] = CardRole.DEAD_Ambassador;
+        break;
+      case CardRole.Contessa:
+        deck[index] = CardRole.DEAD_Contessa;
+        break;
+      default:
+        break;
+    }
+  },
+  /**
+   *
+   * @param val character form of card
+   * @returns Card UI form
+   */
+  getCardFromChar(val: string) {
+    return CardPool.getCard(this.getRoleFromChar(val));
+  },
 
-    },
-    playerHasCard(deck: CardDeck, card: CardRole, player: Player): boolean {
-        const cards = this.peekCards(deck, player.icard, 2);
-        return cards[0] === card || cards[1] === card;
-    },
-    getRoleFromChar(val: string): CardRole {
-        switch (val) {
-            case CardRole.None:
-            case CardRole.Duke:
-            case CardRole.Captain:
-            case CardRole.Assassin:
-            case CardRole.Ambassador:
-            case CardRole.Contessa:
-            case CardRole.DEAD_Captain:
-            case CardRole.DEAD_Duke:
-            case CardRole.DEAD_Assassin:
-            case CardRole.DEAD_Ambassador:
-            case CardRole.DEAD_Contessa:
-                return val as CardRole;
-            default:
-                return CardRole.None;
-        }
-    },
-    killCardAt(deck: CardRole[], index: number) {
-        const role = deck[index];
-        switch (role) {
-            case CardRole.Duke:
-                deck[index] = CardRole.DEAD_Duke;
-                break;
-            case CardRole.Captain:
-                deck[index] = CardRole.DEAD_Captain;
-                break;
-            case CardRole.Assassin:
-                deck[index] = CardRole.DEAD_Assassin;
-                break;
-            case CardRole.Ambassador:
-                deck[index] = CardRole.DEAD_Ambassador;
-                break;
-            case CardRole.Contessa:
-                deck[index] = CardRole.DEAD_Contessa;
-                break;
-            default:
-                break;
-        }
-    },
-    /**
-     *
-     * @param val character form of card
-     * @returns Card UI form
-     */
-    getCardFromChar(val: string) {
-        return CardPool.getCard(this.getRoleFromChar(val));
-    },
+  pushDeck(ctx: RoomContextType, deckArr: CardRole[]) {
+    ReferenceManager.updateReference(DbReferences.GAME_deck, deckArr);
+  },
 
-    pushDeck(ctx: RoomContextType, deckArr: CardRole[]) {
-        ReferenceManager.updateReference(DbReferences.GAME_deck, deckArr);
-    },
+  swap(index1: number, index2: number, deckArr: string[]) {
+    let temp = deckArr[index1];
+    deckArr[index1] = deckArr[index2];
+    deckArr[index2] = temp;
+  },
 
-    swap(index1: number, index2: number, deckArr: string[]) {
-        let temp = deckArr[index1];
-        deckArr[index1] = deckArr[index2];
-        deckArr[index2] = temp;
-    },
+  generateStartingDeck(numPlayers: number): CardRole[] {
+    let numCards = 15;
+    let playerCards = numPlayers * 2;
+    while (numCards - playerCards < 2) {
+      numCards += 5;
+    }
 
-    generateStartingDeck(numPlayers: number): CardRole[] {
-        let numCards = 15;
-        if (numPlayers > 6) numCards = (numPlayers - 6) * 5 + 15;
-        let arr: CardRole[] = []; ////
-        for (let i = 0; i < numCards / 5; i++) {
-            arr.push(CardRole.Duke);
-            arr.push(CardRole.Captain);
-            arr.push(CardRole.Assassin);
-            arr.push(CardRole.Contessa);
-            arr.push(CardRole.Ambassador);
-        }
-        arr = shuffleArray(arr);
-        return arr;
-    },
+    let arr: CardRole[] = []; ////
+    for (let i = 0; i < numCards / 5; i++) {
+      arr.push(CardRole.Duke);
+      arr.push(CardRole.Captain);
+      arr.push(CardRole.Assassin);
+      arr.push(CardRole.Contessa);
+      arr.push(CardRole.Ambassador);
+    }
+    arr = shuffleArray(arr);
+    return arr;
+  },
 
-    peekTopIndex(ctx: RoomContextType): number {
-        let max = 0;
-        const playerMap = ctx.room.playerMap;
-        playerMap.forEach((player) => {
-            max = Math.max(player.icard);
-        });
-        max += 2;
-        return max;
-    },
-    peekCards(
-        deck: CardRole[],
-        startIndex: number,
-        maxNumber: number
-    ): CardRole[] {
-        const maxIndex = Math.min(deck.length, startIndex + maxNumber);
-        const roles: CardRole[] = [];
-        for (let i = startIndex; i < maxIndex; i++) {
-            roles.push(deck[i]);
-        }
-        return roles;
-    },
-    countCards(deck: CardDeck, role: CardRole): number {
-        let counts = 0;
-        deck.forEach((value) => {
-            if (value === role) counts++;
-        });
-        return counts;
-    },
-    playerCardNum(deck: CardRole[], index: number) {
-        let num = 2;
-        const myCards = this.peekCards(deck, index, 2);
-        myCards.forEach((card) => {
-            if (this.isDead(card)) {
-                num--;
-            }
-        });
-        return num;
-    },
+  peekTopIndex(ctx: RoomContextType): number {
+    let max = 0;
+    const playerMap = ctx.room.playerMap;
+    playerMap.forEach((player) => {
+      max = Math.max(player.icard);
+    });
+    max += 2;
+    return max;
+  },
+  peekCards(
+    deck: CardRole[],
+    startIndex: number,
+    maxNumber: number
+  ): CardRole[] {
+    const maxIndex = Math.min(deck.length, startIndex + maxNumber);
+    const roles: CardRole[] = [];
+    for (let i = startIndex; i < maxIndex; i++) {
+      roles.push(deck[i]);
+    }
+    return roles;
+  },
+  countCards(deck: CardDeck, role: CardRole): number {
+    let counts = 0;
+    deck.forEach((value) => {
+      if (value === role) counts++;
+    });
+    return counts;
+  },
+  playerCardNum(deck: CardRole[], index: number) {
+    let num = 2;
+    const myCards = this.peekCards(deck, index, 2);
+    myCards.forEach((card) => {
+      if (this.isDead(card)) {
+        num--;
+      }
+    });
+    return num;
+  },
 };
