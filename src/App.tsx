31a4724005e2367ef888c95e294d52a708f30fb5
Zerstorer23 <bismark439@gmail.com>
2022-06-04 11:39:34 -0500

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
 import Lobby from "./pages/lobby/Lobby";
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import InGame from "./pages/ingame/InGame";
 import DataLoader from "pages/DataLoader/DataLoader";
 import MyTimer from "pages/testings/Timer";
 
 export default function App(): JSX.Element {
-    return (
-        <DataLoader>
-            <Switch>
-                <Route path="/" exact>
-                    <p>Loading...</p>
-                </Route>
-                <Route path="/lobby" exact>
-                    <Lobby/>
-                </Route>
-                <Route path="/game" exact>
-                    <InGame/>
-                </Route>
-                <Route path="/timer" exact>
-                    <MyTimer/>
-                </Route>
-                <Route path="/tutorial" exact>
-                    <p>Tutorial page</p>
-                </Route>
-                <Route path="/finished" exact>
-                    <p>Winner page</p>
-                </Route>
-                <Route path="*">
-                    <p>Not found</p>
-                </Route>
-            </Switch>
-        </DataLoader>
-    );
+  return (
+    <DataLoader>
+      <Switch>
+        <Route path="/suzumiya/" exact>
+          <p>Loading...</p>
+        </Route>
+        <Route path="/suzumiya/lobby" exact>
+          <Lobby />
+        </Route>
+        <Route path="/suzumiya/game" exact>
+          <InGame />
+        </Route>
+        <Route path="/suzumiya/timer" exact>
+          <MyTimer />
+        </Route>
+        <Route path="/suzumiya/tutorial" exact>
+          <p>Tutorial page</p>
+        </Route>
+        <Route path="/suzumiya/finished" exact>
+          <p>Winner page</p>
+        </Route>
+        <Route path="*">
+          <p>Not found</p>
+        </Route>
+      </Switch>
+    </DataLoader>
+  );
 }
