b1cbeed56d58ea24bb0384d02878e575fcd4fe79
jzhang2254 <jzhang2254@wisc.edu>
2022-06-05 18:31:10 -0500

    Sun 06-05-2022 _06:31 PM

--- src/pages/ingame/Center/ActionBoards/Boards/AmbassadorBoard.tsx
+++ src/pages/ingame/Center/ActionBoards/Boards/AmbassadorBoard.tsx
@@ -1,14 +1,14 @@
 import classes from "pages/ingame/Center/ActionBoards/Boards/BaseBoard.module.css";
 import RoomContext from "system/context/roomInfo/room-context";
-import {Fragment, useContext, useEffect, useState} from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import LocalContext from "system/context/localInfo/local-context";
-import {DeckManager} from "system/cards/DeckManager";
-import {Card, CardRole} from "system/cards/Card";
+import { DeckManager } from "system/cards/DeckManager";
+import { Card, CardRole } from "system/cards/Card";
 import BaseActionButton from "pages/ingame/Center/ActionBoards/Boards/ActionButtons/BaseActionButton";
-import {TurnManager} from "system/GameStates/TurnManager";
+import { TurnManager } from "system/GameStates/TurnManager";
 import * as ActionManager from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
-import {TransitionAction} from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
-import {useShortcutEffect} from "system/hooks/useShortcut";
+import { TransitionAction } from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
+import { useShortcutEffect } from "system/hooks/useShortcut";
 
 export default function AmbassadorBoard(): JSX.Element {
   const ctx = useContext(RoomContext);
@@ -20,20 +20,19 @@ export default function AmbassadorBoard(): JSX.Element {
   const topIndex = DeckManager.peekTopIndex(ctx);
   const [firstCardPicked, setFirstCardPicked] = useState<number>(-1);
   const [cardArr, setCardArr] = useState<Card[]>(
-      [
-        ...DeckManager.peekCards(deck, myPlayer.icard, 2),
-        ...DeckManager.peekCards(deck, topIndex, 2),
-      ].map((val) => {
-        return DeckManager.getCardFromChar(val);
-      })
+    [
+      ...DeckManager.peekCards(deck, myPlayer.icard, 2),
+      ...DeckManager.peekCards(deck, topIndex, 2),
+    ].map((val) => {
+      return DeckManager.getCardFromChar(val);
+    })
   );
   const keyInfo = useShortcutEffect(cardArr.length);
   useEffect(() => {
     const idx = keyInfo.index;
     //TODO
-    onMakeAction(idx);
-  }, [keyInfo])
-
+    if (idx !== -1) onMakeAction(idx);
+  }, [keyInfo]);
 
   /**************************************************************
    * Handle the case where deck only has 0 ~ 2 cards available  *
@@ -42,6 +41,7 @@ export default function AmbassadorBoard(): JSX.Element {
   //TODO: Dead Card
   useEffect(() => {
     if (DeckManager.playerCardNum(deck, myPlayer.icard) === 1) {
+      console.log("one card alive");
       //swap
       if (firstCardPicked === 1) {
         DeckManager.swap(myPlayer!.icard + 1, myPlayer!.icard, deck);
@@ -62,8 +62,9 @@ export default function AmbassadorBoard(): JSX.Element {
 
   //test
   useEffect(() => {
-    if (count !== 2) return;
+    if (count < 2) return;
     if (secondCardPicked === firstCardPicked) return;
+    console.log("swapping");
     if (firstCardPicked === 1) {
       DeckManager.swap(myPlayer!.icard + 1, myPlayer!.icard, deck);
     } else if (firstCardPicked === 2) {
@@ -94,6 +95,8 @@ export default function AmbassadorBoard(): JSX.Element {
   }, [firstCardPicked, secondCardPicked]);
 
   const onMakeAction = (index: number) => {
+    console.log("index: ", index);
+    console.log("count: ", count);
     if (count === 0) setFirstCardPicked(index);
     if (count === 1) setSecondCardPicked(index);
     setCount((count: number) => {
@@ -103,54 +106,54 @@ export default function AmbassadorBoard(): JSX.Element {
 
   //ADD SELCETED CSS STYLE
   return (
-      <Fragment>
-        <div className={classes.header}>
-          Choose 2 cards that you want to keep...
-        </div>
-        <p>mine</p>
-        <div className={classes.quarterContainer}>
-          {cardArr.map((action: Card, index: number) => {
-            if (firstCardPicked === index) {
-              return <Fragment/>;
-            }
-            if (index > 1) {
-              return <Fragment key={index}/>;
-            }
+    <Fragment>
+      <div className={classes.header}>
+        Choose 2 cards that you want to keep...
+      </div>
+      <p>mine</p>
+      <div className={classes.quarterContainer}>
+        {cardArr.map((action: Card, index: number) => {
+          if (firstCardPicked === index) {
+            return <Fragment />;
+          }
+          if (index > 1) {
+            return <Fragment key={index} />;
+          }
 
-            return (
-                <BaseActionButton
-                    key={index}
-                    index={index}
-                    param={action}
-                    onClickButton={() => {
-                      onMakeAction(index);
-                    }}
-                />
-            );
-          })}
-        </div>
-        <p>deck</p>
+          return (
+            <BaseActionButton
+              key={index}
+              index={index}
+              param={action}
+              onClickButton={() => {
+                onMakeAction(index);
+              }}
+            />
+          );
+        })}
+      </div>
+      <p>deck</p>
 
-        <div className={classes.quarterContainer}>
-          {cardArr.map((action: Card, index: number) => {
-            if (firstCardPicked === index) {
-              return <Fragment/>;
-            }
-            if (index < 2) {
-              return <Fragment key={index}/>;
-            }
-            return (
-                <BaseActionButton
-                    key={index}
-                    index={index}
-                    param={action}
-                    onClickButton={() => {
-                      onMakeAction(index);
-                    }}
-                />
-            );
-          })}
-        </div>
-      </Fragment>
+      <div className={classes.quarterContainer}>
+        {cardArr.map((action: Card, index: number) => {
+          if (firstCardPicked === index) {
+            return <Fragment />;
+          }
+          if (index < 2) {
+            return <Fragment key={index} />;
+          }
+          return (
+            <BaseActionButton
+              key={index}
+              index={index}
+              param={action}
+              onClickButton={() => {
+                onMakeAction(index);
+              }}
+            />
+          );
+        })}
+      </div>
+    </Fragment>
   );
 }
