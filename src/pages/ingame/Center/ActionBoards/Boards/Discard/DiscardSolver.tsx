8c2a58fec2af4a09e1ee5e7bb74ab38f44030a2a
jzhang2254 <jzhang2254@wisc.edu>
2022-06-06 18:04:59 -0500

    Mon 06-06-2022 _06:04 PM

--- src/pages/ingame/Center/ActionBoards/Boards/Discard/DiscardSolver.tsx
+++ src/pages/ingame/Center/ActionBoards/Boards/Discard/DiscardSolver.tsx
@@ -1,139 +1,145 @@
-import {LocalContextType} from "system/context/localInfo/local-context";
-import {KillInfo} from "system/GameStates/GameTypes";
-import {TurnManager} from "system/GameStates/TurnManager";
+import { LocalContextType } from "system/context/localInfo/local-context";
+import { KillInfo } from "system/GameStates/GameTypes";
+import { TurnManager } from "system/GameStates/TurnManager";
 import * as ActionManager from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
-import {TransitionAction} from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
-import {setMyTimer} from "pages/components/ui/MyTimer/MyTimer";
-import {WaitTime} from "system/GameConstants";
-import {BoardState} from "system/GameStates/States";
-import {MyCardsPanel,} from "pages/ingame/Center/ActionBoards/Boards/Discard/DiscardPanels";
-import {DeckManager} from "system/cards/DeckManager";
-import {DbReferences, ReferenceManager} from "system/Database/RoomDatabase";
-import {DS} from "system/Debugger/DS";
-import {RoomContextType} from "system/context/roomInfo/RoomContextProvider";
+import { TransitionAction } from "pages/ingame/Center/ActionBoards/StateManagers/TransitionManager";
+import { setMyTimer } from "pages/components/ui/MyTimer/MyTimer";
+import { WaitTime } from "system/GameConstants";
+import { BoardState } from "system/GameStates/States";
+import { MyCardsPanel } from "pages/ingame/Center/ActionBoards/Boards/Discard/DiscardPanels";
+import { DeckManager } from "system/cards/DeckManager";
+import { DbReferences, ReferenceManager } from "system/Database/RoomDatabase";
+import { DS } from "system/Debugger/DS";
+import { RoomContextType } from "system/context/roomInfo/RoomContextProvider";
 import WaitingPanel from "pages/ingame/Center/ActionBoards/Boards/Waiter/WaitingPanel";
+import { ChatFormat, ChatProvider } from "system/context/chatInfo/ChatContextProvider";
 
 export function handleDiscardState(
-    ctx: RoomContextType,
-    localCtx: LocalContextType,
-    killInfo: KillInfo
+  ctx: RoomContextType,
+  localCtx: LocalContextType,
+  killInfo: KillInfo
 ): JSX.Element {
-    const [myId] = TurnManager.getMyInfo(ctx, localCtx);
-    if (myId === killInfo.ownerId) {
-        return handleLoserTurn(ctx, localCtx, killInfo);
-    } else {
-        return handleOtherTurn(ctx, localCtx, killInfo);
-    }
+  const [myId] = TurnManager.getMyInfo(ctx, localCtx);
+  if (myId === killInfo.ownerId) {
+    return handleLoserTurn(ctx, localCtx, killInfo);
+  } else {
+    return handleOtherTurn(ctx, localCtx, killInfo);
+  }
 }
 
 function handleLoserTurn(
-    ctx: RoomContextType,
-    localCtx: LocalContextType,
-    killInfo: KillInfo
+  ctx: RoomContextType,
+  localCtx: LocalContextType,
+  killInfo: KillInfo
 ): JSX.Element {
-    if (killInfo.nextState === BoardState.CalledAssassinate) {
-        const numAlive = DeckManager.playerAliveCardNum(ctx.room.game.deck, ctx.room.playerMap.get(killInfo.ownerId)!.icard);
-        if (numAlive === 2) {
-            handleSuicide(ctx, killInfo.ownerId);
-            return <WaitingPanel/>;
-        }
+  if (killInfo.nextState === BoardState.CalledAssassinate) {
+    const numAlive = DeckManager.playerAliveCardNum(
+      ctx.room.game.deck,
+      ctx.room.playerMap.get(killInfo.ownerId)!.icard
+    );
+    if (numAlive === 2) {
+      handleSuicide(ctx, killInfo.ownerId);
+      return <WaitingPanel />;
     }
-    if (killInfo.removed[0] < 0) {
-        return <MyCardsPanel/>;
-    } else {
-        return <WaitingPanel/>;
-    }
+  }
+  if (killInfo.removed[0] < 0) {
+    return <MyCardsPanel />;
+  } else {
+    return <WaitingPanel />;
+  }
 }
 
 function handleOtherTurn(
-    ctx: RoomContextType,
-    localCtx: LocalContextType,
-    killInfo: KillInfo
+  ctx: RoomContextType,
+  localCtx: LocalContextType,
+  killInfo: KillInfo
 ): JSX.Element {
-    if (killInfo.removed[0] < 0) {
-        return <WaitingPanel/>;
-    } else {
-        hostEndsState(ctx, localCtx, killInfo);
-        return <WaitingPanel/>;
+  if (killInfo.removed[0] < 0) {
+    return <WaitingPanel />;
+  } else {
+    hostEndsState(ctx, localCtx, killInfo);
+    return <WaitingPanel />;
+  }
+}
+
+function hostEndsState(
+  ctx: RoomContextType,
+  localCtx: LocalContextType,
+  killInfo: KillInfo
+) {
+  const amHost = TurnManager.amHost(ctx, localCtx);
+  if (!amHost) return;
+
+  //TODO Host handles ending discarding state.
+  setMyTimer(localCtx, WaitTime.WaitConfirms, () => {
+    const nextBoard = killInfo.nextState as BoardState;
+    //TODO if alive player number <= 1, set turn -2 . return TransitionAction.EndGame
+    const winnerId = DeckManager.checkGameOver(ctx);
+    if (winnerId !== "") {
+      ActionManager.pushEndGame(ctx, winnerId);
+      return;
     }
+    //else do next board switch
+    switch (nextBoard) {
+      case BoardState.GetThreeAccepted:
+      case BoardState.AmbassadorAccepted:
+      case BoardState.StealAccepted:
+      case BoardState.ForeignAidAccepted:
+        ActionManager.prepareAndPushState(ctx, (newAction, newState) => {
+          newState.board = nextBoard;
+          return TransitionAction.Success;
+        });
+        break;
+      case BoardState.CalledAssassinate:
+      default:
+        ActionManager.prepareAndPushState(ctx, (newAction, newState) => {
+          return TransitionAction.EndTurn;
+        });
+        break;
+    }
+  });
 }
-
-function hostEndsState(ctx: RoomContextType,
-                       localCtx: LocalContextType,
-                       killInfo: KillInfo) {
-    const amHost = TurnManager.amHost(ctx, localCtx);
-    if (!amHost) return;
-
-    //TODO Host handles ending discarding state.
-    setMyTimer(localCtx, WaitTime.WaitConfirms, () => {
-        const nextBoard = killInfo.nextState as BoardState;
-        //TODO if alive player number <= 1, set turn -2 . return TransitionAction.EndGame
-        const winnerId = null;
-        if (winnerId !== null) {
-            ActionManager.pushEndGame(ctx, winnerId);
-            return;
-        }
-        //else do next board switch
-        switch (nextBoard) {
-            case BoardState.GetThreeAccepted:
-            case BoardState.AmbassadorAccepted:
-            case BoardState.StealAccepted:
-            case BoardState.ForeignAidAccepted:
-                ActionManager.prepareAndPushState(ctx, (newAction, newState) => {
-                    newState.board = nextBoard;
-                    return TransitionAction.Success;
-                });
-                break;
-            case BoardState.CalledAssassinate:
-            default:
-                ActionManager.prepareAndPushState(ctx, (newAction, newState) => {
-                    return TransitionAction.EndTurn;
-                });
-                break;
-        }
-    });
-}
-
 
 export function handleCardKill(ctx: RoomContextType, index: number) {
-    const deck = ctx.room.game.deck;
-    DeckManager.killCardAt(deck, index);
-    ActionManager.prepareAndPushState(ctx, (newAction) => {
-        const killedInfo = newAction.param as KillInfo;
-        killedInfo.removed[0] = index;
-        newAction.param = killedInfo;
-        ReferenceManager.updateReference(DbReferences.GAME_deck, deck);
-        const isDead = DeckManager.playerIsDead(
-            deck,
-            ctx.room.playerMap.get(killedInfo.ownerId)!
-        );
-        if (isDead) {
-            const player = ctx.room.playerMap.get(killedInfo.ownerId)!;
-            player.isSpectating = true;
-            player.coins = 0;
-            ReferenceManager.updatePlayerReference(killedInfo.ownerId, player);
-        }
-        //If it was, set spectating on
-        DS.logTransition("Removed card at " + index);
-        DS.logTransition(newAction);
-        return TransitionAction.Success;
-    });
+  const deck = ctx.room.game.deck;
+  DeckManager.killCardAt(deck, index);
+  ActionManager.prepareAndPushState(ctx, (newAction) => {
+    const killedInfo = newAction.param as KillInfo;
+    killedInfo.removed[0] = index;
+    newAction.param = killedInfo;
+    ReferenceManager.updateReference(DbReferences.GAME_deck, deck);
+    const isDead = DeckManager.playerIsDead(
+      deck,
+      ctx.room.playerMap.get(killedInfo.ownerId)!
+    );
+    if (isDead) {
+      const player = ctx.room.playerMap.get(killedInfo.ownerId)!;
+      player.isSpectating = true;
+      player.coins = 0;
+      ReferenceManager.updatePlayerReference(killedInfo.ownerId, player);
+      ChatProvider.sendChat(ChatFormat.important, "", $The player is removed from the game);
+    }
+    //If it was, set spectating on
+    DS.logTransition("Removed card at " + index);
+    DS.logTransition(newAction);
+    return TransitionAction.Success;
+  });
 }
 
 export function handleSuicide(ctx: RoomContextType, playerId: string) {
-    const deck = ctx.room.game.deck;
-    ActionManager.prepareAndPushState(ctx, (newAction) => {
-        const player = ctx.room.playerMap.get(playerId)!;
-        DeckManager.killCardAt(deck, player.icard);
-        DeckManager.killCardAt(deck, player.icard + 1);
-        const killedInfo = newAction.param as KillInfo;
-        killedInfo.removed[0] = player.icard;
-        killedInfo.removed[1] = player.icard + 1;
-        newAction.param = killedInfo;
-        ReferenceManager.updateReference(DbReferences.GAME_deck, deck);
-        player.isSpectating = true;
-        player.coins = 0;
-        ReferenceManager.updatePlayerReference(killedInfo.ownerId, player);
-        return TransitionAction.Success;
-    });
+  const deck = ctx.room.game.deck;
+  ActionManager.prepareAndPushState(ctx, (newAction) => {
+    const player = ctx.room.playerMap.get(playerId)!;
+    DeckManager.killCardAt(deck, player.icard);
+    DeckManager.killCardAt(deck, player.icard + 1);
+    const killedInfo = newAction.param as KillInfo;
+    killedInfo.removed[0] = player.icard;
+    killedInfo.removed[1] = player.icard + 1;
+    newAction.param = killedInfo;
+    ReferenceManager.updateReference(DbReferences.GAME_deck, deck);
+    player.isSpectating = true;
+    player.coins = 0;
+    ReferenceManager.updatePlayerReference(killedInfo.ownerId, player);
+    return TransitionAction.Success;
+  });
 }
--- src/pages/ingame/InGame.tsx
+++ src/pages/ingame/InGame.tsx
@@ -7,82 +7,95 @@ import MainTableBoard from "./Center/MainTableBoard/MainTableBoard";
 import ActionBoards from "./Center/ActionBoards/ActionBoards";
 import GameDeckBoard from "./Right/GameDeckBoard/GameDeckBoard";
 import InGameChatBoard from "./Right/ChatBoard/InGameChatBoard";
-import {useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import RoomContext from "system/context/roomInfo/room-context";
-import LocalContext, {LocalField,} from "system/context/localInfo/local-context";
-import {useHistory} from "react-router-dom";
-import {DbReferences, ReferenceManager} from "system/Database/RoomDatabase";
+import LocalContext, {
+  LocalField,
+} from "system/context/localInfo/local-context";
+import { useHistory } from "react-router-dom";
+import { DbReferences, ReferenceManager } from "system/Database/RoomDatabase";
 import GameOverPopUp from "pages/components/ui/PopUp/PopUp";
-import {Navigation} from "App";
-import {TurnManager} from "system/GameStates/TurnManager";
+import { Navigation } from "App";
+import { TurnManager } from "system/GameStates/TurnManager";
+import { setMyTimer } from "pages/components/ui/MyTimer/MyTimer";
+import { WaitTime } from "system/GameConstants";
+import { TurnState } from "system/GameStates/GameTypes";
 
 export default function InGame() {
-    const ctx = useContext(RoomContext);
-    const localCtx = useContext(LocalContext);
-    const history = useHistory();
-    const myId = localCtx.getVal(LocalField.Id);
-    const [roomCode, setRoomCode] = useState<number>(0);
-    const [isGameOver, setIsGameOver] = useState<boolean>(false);
-    const amHost = TurnManager.amHost(ctx, localCtx);
+  const ctx = useContext(RoomContext);
+  const localCtx = useContext(LocalContext);
+  const history = useHistory();
+  const myId = localCtx.getVal(LocalField.Id);
+  const [roomCode, setRoomCode] = useState<number>(0);
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
+  const amHost = TurnManager.amHost(ctx, localCtx);
 
-    function checkSanity() {
-        if (myId === ctx.room.header.hostId) {
-            ReferenceManager.updateReference(
-                DbReferences.GAME_action,
-                ctx.room.game.action
-            );
-            ReferenceManager.updateReference(
-                DbReferences.GAME_state,
-                ctx.room.game.state
-            );
-            ReferenceManager.updateReference(DbReferences.HEADER_hostId, myId);
-        }
+  function checkSanity() {
+    if (myId === ctx.room.header.hostId) {
+      ReferenceManager.updateReference(
+        DbReferences.GAME_action,
+        ctx.room.game.action
+      );
+      ReferenceManager.updateReference(
+        DbReferences.GAME_state,
+        ctx.room.game.state
+      );
+      ReferenceManager.updateReference(DbReferences.HEADER_hostId, myId);
     }
+  }
 
-    useEffect(() => {
-        checkSanity();
-        setRoomCode((n) => n++);
-    }, [ctx.room.playerMap.size]);
+  useEffect(() => {
+    checkSanity();
+    setRoomCode((n) => n++);
+  }, [ctx.room.playerMap.size]);
 
-    useEffect(() => {
-        if (myId === null) {
-            history.replace(Navigation.Loading);
-        }
-    }, [myId, history]);
-    const turn = ctx.room.game.state.turn;
-    useEffect(() => {
-        if (turn === -2) {
-            setIsGameOver(true);
-            //TODO
-            //Set timer 5 sec,
-            //If I am host,  change turn on expire
-        } else if (turn === -1) {
-            setIsGameOver(false);
-            history.replace(Navigation.Lobby);
-        }
-    }, [turn]);
-
+  useEffect(() => {
     if (myId === null) {
-        return <p>Loading...</p>;
+      history.replace(Navigation.Loading);
     }
+  }, [myId, history]);
+  const turn = ctx.room.game.state.turn;
+  useEffect(() => {
+    if (turn === -2) {
+      setIsGameOver(true);
+      //Set timer 5 sec,
+      //If I am host,  change turn on expire
+      setMyTimer(localCtx, WaitTime.WaitReactions, () => {
+        if (amHost) {
+          const state: TurnState = {
+            turn: -1,
+            board: 0,
+          };
+          ReferenceManager.updateReference(DbReferences.GAME_state, state);
+        }
+      });
+    } else if (turn === -1) {
+      setIsGameOver(false);
+      history.replace(Navigation.Lobby);
+    }
+  }, [turn]);
 
-    return (
-        <div className={classes.container}>
-            {isGameOver && <GameOverPopUp/>}
-            <HorizontalLayout>
-                <VerticalLayout className={`${classes.leftPanel}`}>
-                    <PlayerBoard/>
-                    <MyBoard/>
-                </VerticalLayout>
-                <VerticalLayout className={`${classes.centerPanel}`}>
-                    <MainTableBoard/>
-                    <ActionBoards code={roomCode}/>
-                </VerticalLayout>
-                <VerticalLayout className={`${classes.rightPanel}`}>
-                    <GameDeckBoard/>
-                    <InGameChatBoard/>
-                </VerticalLayout>
-            </HorizontalLayout>
-        </div>
-    );
+  if (myId === null) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className={classes.container}>
+      {isGameOver && <GameOverPopUp />}
+      <HorizontalLayout>
+        <VerticalLayout className={`${classes.leftPanel}`}>
+          <PlayerBoard />
+          <MyBoard />
+        </VerticalLayout>
+        <VerticalLayout className={`${classes.centerPanel}`}>
+          <MainTableBoard />
+          <ActionBoards code={roomCode} />
+        </VerticalLayout>
+        <VerticalLayout className={`${classes.rightPanel}`}>
+          <GameDeckBoard />
+          <InGameChatBoard />
+        </VerticalLayout>
+      </HorizontalLayout>
+    </div>
+  );
 }
