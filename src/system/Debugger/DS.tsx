1bf2da316b52bf8d423ba46fb836809fbee07fdd
Zerstorer23 <bismark439@gmail.com>
2022-06-08 19:18:38 -0500

    Better clean chat

--- src/system/Debugger/DS.tsx
+++ src/system/Debugger/DS.tsx
@@ -1,7 +1,7 @@
 export const DS = {
     StrictRules: true,
     serverSG: true,
-    abundantCoins: true,
+    abundantCoins: false,
     logTransition(msg: any) {
         console.log(msg);
     }
