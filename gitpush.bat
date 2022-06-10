c23fb2e22eb47ba9fd04affa14f87416852f3517
Zerstorer23 <bismark439@gmail.com>
2022-06-09 20:26:24 -0500

    2022-06-09 _오후 08:26

--- gitpush.bat
+++ gitpush.bat
@@ -1,4 +1,3 @@
-npm run deploy
 For /f "tokens=1-3 delims=/-" %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
 SET timestr=%mydate%_%mytime%
