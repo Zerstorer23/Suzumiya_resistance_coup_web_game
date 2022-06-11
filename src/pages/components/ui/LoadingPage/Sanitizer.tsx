43669e9cb62f152a658d08814e0c8e107b0b06ba
Zerstorer23 <bismark439@gmail.com>
2022-06-11 03:43:24 -0500

    lobby bug host fix

--- src/pages/components/ui/LoadingPage/Sanitizer.tsx
+++ src/pages/components/ui/LoadingPage/Sanitizer.tsx
@@ -1,41 +1,61 @@
-import { IProps } from "system/types/CommonTypes";
-import { Fragment, useContext, useEffect, useState } from "react";
+import {IProps} from "system/types/CommonTypes";
+import {Fragment, useContext, useEffect, useState} from "react";
 import RoomContext from "system/context/roomInfo/room-context";
-import LocalContext, {
-  LocalField,
-} from "system/context/localInfo/local-context";
-import { useHistory } from "react-router-dom";
-import { Navigation } from "App";
+import LocalContext, {LocalField} from "system/context/localInfo/local-context";
+import {useHistory} from "react-router-dom";
+import {Navigation} from "App";
+import {TurnManager} from "system/GameStates/TurnManager";
+import {DbFields, ReferenceManager} from "system/Database/ReferenceManager";
 
 export default function Sanitizer(props: IProps) {
-  const ctx = useContext(RoomContext);
-  const localCtx = useContext(LocalContext);
-  const history = useHistory();
-  const myId = localCtx.getVal(LocalField.Id);
-  const turns = ctx.room.game.state.turn;
+    const ctx = useContext(RoomContext);
+    const localCtx = useContext(LocalContext);
+    const history = useHistory();
+    const myId = localCtx.getVal(LocalField.Id);
+    const turns = ctx.room.game.state.turn;
+    const amHost = TurnManager.amHost(ctx, localCtx);
+    const myEntry = TurnManager.getMyInfo(ctx, localCtx);
 
-  //   useEffect(() => {
-  //     connect();
-  //   }, []);
+    function checkSanity(): boolean {
+        if (!amHost) return false;
+        ReferenceManager.updateReference(
+            DbFields.GAME_action,
+            ctx.room.game.action
+        );
+        ReferenceManager.updateReference(
+            DbFields.GAME_state,
+            ctx.room.game.state
+        );
+        ReferenceManager.updateReference(DbFields.HEADER_hostId, myEntry.id);
+        return true;
+    }
 
-  const [valid, setValid] = useState(false);
-  useEffect(() => {
-    if (myId === null) {
-      setValid(false);
-      history.replace(Navigation.Loading);
-      return;
-    }
-    if (!ctx.room.playerMap.has(myId)) {
-      setValid(false);
-      localCtx.setVal(LocalField.Id, null);
-      history.replace(Navigation.Loading);
-      return;
-    }
-    setValid(true);
-    if (turns >= 0) {
-      history.replace(Navigation.InGame);
-      return;
-    }
-  }, [turns, myId]);
-  return <Fragment>{valid && props.children}</Fragment>;
+    useEffect(() => {
+        checkSanity();
+    }, [ctx.room.playerMap.size, amHost]);
+
+    const [valid, setValid] = useState(false);
+    useEffect(() => {
+        if (myId === null) {
+            setValid(false);
+            history.replace(Navigation.Loading);
+            return;
+        }
+        if (!ctx.room.playerMap.has(myId)) {
+            setValid(false);
+            localCtx.setVal(LocalField.Id, null);
+            history.replace(Navigation.Loading);
+            return;
+        }
+        setValid(true);
+        if (turns >= 0) {
+            history.replace(Navigation.InGame);
+            return;
+        }
+    }, [turns, myId]);
+    return <Fragment>
+        {(valid) &&
+            props.children
+        }
+    </Fragment>;
 }
--- src/pages/lobby/Lobby.tsx
+++ src/pages/lobby/Lobby.tsx
@@ -4,8 +4,14 @@ import classes from "./Lobby.module.css";
 import LobbySettings from "./Left/LobbySettings";
 import Sanitizer from "pages/components/ui/LoadingPage/Sanitizer";
 import gc from "global.module.css";
+import {useContext} from "react";
+import RoomContext from "system/context/roomInfo/room-context";
+import LocalContext from "system/context/localInfo/local-context";
 
 export default function Lobby() {
+    const ctx = useContext(RoomContext);
+    const localCtx = useContext(LocalContext);
+
     return (<Sanitizer>
         <div className={`${classes.container} ${gc.panelBackground}`}>
             <LobbySettings/>
